56a3a4f811642324a7f2557add454b1c6d3fb0da
Abhinav Verma <71164570+vermaabhinav313@users.noreply.github.com>
2021-06-09 15:25:20 +0530

    Update userinfo.py

--- LEGEND/modules/userinfo.py
+++ LEGEND/modules/userinfo.py
@@ -242,22 +242,19 @@ def info(update: Update, context: CallbackContext):
     rep = message.reply_text("<code>Appraising...</code>", parse_mode=ParseMode.HTML)
 
     text = (
-        f"╒═══「<b> Appraisal results:</b> 」\n"
+        f"<b> User Information:</b> \n"
         f"ID: <code>{user.id}</code>\n"
         f"First Name: {html.escape(user.first_name)}"
     )
 
     if user.last_name:
         text += f"\nLast Name: {html.escape(user.last_name)}"
-
     if user.username:
         text += f"\nUsername: @{html.escape(user.username)}"
 
-    text += f"\nPermalink: {mention_html(user.id, 'link')}"
-
+    text += f"\n<b>Permalink</b>: {mention_html(user.id, 'link')}"
     if chat.type != "private" and user_id != bot.id:
         _stext = "\nPresence: <code>{}</code>"
-
         afk_st = is_afk(user.id)
         if afk_st:
             text += _stext.format("AFK")
