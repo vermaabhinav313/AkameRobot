e085431fe0eb809b82eb7b4cad595f5a82826890
Abhinav Verma <71164570+vermaabhinav313@users.noreply.github.com>
2021-06-17 11:27:28 +0530

    Update afk.py

--- LEGEND/modules/afk.py
+++ LEGEND/modules/afk.py
@@ -4,10 +4,10 @@ from telegram import Message, Update, Bot, User
 from telegram import MessageEntity
 from telegram.ext import Filters, MessageHandler, run_async
 
-from tg_bot import dispatcher
-from tg_bot.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHandler
-from tg_bot.modules.sql import afk_sql as sql
-from tg_bot.modules.users import get_user_id
+from LEGEND import dispatcher
+from LEGEND.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHandler
+from LEGEND.modules.sql import afk_sql as sql
+from LEGEND.modules.users import get_user_id
 
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
